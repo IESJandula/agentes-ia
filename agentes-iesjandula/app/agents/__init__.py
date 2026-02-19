cb276ca915e65828eacfe6e309fede04e12310d5
AntonioDavid333 <adarte82899@gmail.com>
2026-02-19 13:10:23 +0100

    experimento-agente-voz-01

--- agentes-iesjandula/app/agents/__init__.py
+++ agentes-iesjandula/app/agents/__init__.py
@@ -1,3 +1,4 @@
 from .agente_profesores import inicializar_agente_profesores
+from .agente_profesores_voz import AgenteVozJandula
 
-__all__ = ["inicializar_agente_profesores"]
+__all__ = ["inicializar_agente_profesores", "AgenteVozJandula"]
